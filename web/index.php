ed2066fa77d5bd89e38a78816d4ba782537a3883
Charles Thomas <chuckbill@msn.com>
2021-04-07 20:10:33 -0400

    update

--- web/index.php
+++ web/index.php
@@ -1,25 +1,34 @@
 <?php
-
 require('../vendor/autoload.php');
+$servername = "us-cdbr-east-03.cleardb.com";
+$username = "b187614a16d726";
+$password = "950b4df7";
+//mysql://b187614a16d726:950b4df7@us-cdbr-east-03.cleardb.com/heroku_01356cb77fdb7dd?reconnect=true
+// Create connection
+$conn = new mysqli($servername, $username, $password);
 
-$app = new Silex\Application();
-$app['debug'] = true;
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+$sql = "SELECT id, name, str, dex FROM player";
+$result = $conn->query($sql);
 
-// Register the monolog logging service
-$app->register(new Silex\Provider\MonologServiceProvider(), array(
-  'monolog.logfile' => 'php://stderr',
-));
 
-// Register view rendering
-$app->register(new Silex\Provider\TwigServiceProvider(), array(
-    'twig.path' => __DIR__.'/views',
-));
+?>
 
-// Our web handlers
-
-$app->get('/', function() use($app) {
-  $app['monolog']->addDebug('logging output.');
-  return $app['twig']->render('index.twig');
-});
-
-$app->run();
+<html>
+  <title>Hello Worldx</title>
+  <body><?php 
+    if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    echo "Name: " . $row["name"]. " - Str: " . $row["str"]. " - Dex: " . $row["dex"]. "<br>";
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
+    
+    ?></body>
+</html>
